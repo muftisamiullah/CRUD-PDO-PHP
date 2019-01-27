b88807a355b88ac0c04f2775e1b1b6c4209dbb25
Sami Ullah <muftisami2010@gmail.com>
2019-01-27 16:52:08 +0530

    corrected the mistake in .config file

--- config.php
+++ config.php
@@ -5,12 +5,12 @@
  *
  */
 
-$host       = "localhost";
-$username   = "muftisamiullah";
-$password   = "letusc123";
-$dbname     = "testing";
+$host       = "localhost"; 
+$username   = "muftisamiullah"; //username of db
+$password   = "letusc123";  //password of db
+$dbname     = "testing";  //name of db
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //make the default fetch be an associative array
               );
-
